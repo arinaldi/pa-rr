95f2ef2633f85158c9cfc8cfb8f76aa7b267d9c6
Tony Rinaldi <tony@grazzy.com>
2025-04-08 15:00:39 -0500

    update not found component

--- src/routes/not-found.tsx
+++ src/routes/not-found.tsx
@@ -2,7 +2,7 @@ import { Link2OffIcon } from 'lucide-react';
 
 export default function NotFound() {
   return (
-    <div className="m-5 flex items-center justify-center">
+    <div className="max-w-sm">
       <div className="flex w-full flex-col items-center justify-center rounded-md border border-dashed p-6 text-center sm:w-auto sm:min-w-[400px]">
         <Link2OffIcon className="text-muted-foreground size-10" />
         <h1 className="mt-4 text-4xl font-semibold">404</h1>

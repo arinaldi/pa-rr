4ac75ff7d6b5d4becf948c46a962ec257b10588a
Tony Rinaldi <tony@grazzy.com>
2025-04-14 10:44:50 -0500

    inset sidebar with bottom border on shortened header

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -35,7 +35,7 @@ export function AppSidebar() {
 
   return (
     <SidebarProvider>
-      <Sidebar>
+      <Sidebar variant="inset">
         <SidebarHeader>
           <SidebarMenu>
             <SidebarMenuItem>
@@ -92,7 +92,7 @@ export function AppSidebar() {
         <SidebarRail />
       </Sidebar>
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="flex h-12 shrink-0 items-center gap-2 border-b-1 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator
@@ -109,7 +109,7 @@ export function AppSidebar() {
             </Breadcrumb>
           </div>
         </header>
-        <div className="isolate p-4 pt-0">
+        <div className="isolate p-4">
           <Outlet />
         </div>
       </SidebarInset>

869a81f42c629330f3a80dd1495d9bb2e9912331
Tony Rinaldi <tony.rinaldi@grocerytv.com>
2026-02-03 08:46:04 -0600

    code review

--- src/components/private-route.tsx
+++ src/components/private-route.tsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from 'react-router';
 
 import { useSession } from '@/components/session-provider';
+import { ROUTE_HREF } from '@/lib/constants';
 
 export function PrivateRoute() {
   const session = useSession();
 
   if (!session) {
-    return <Navigate to="/not-found" />;
+    return <Navigate to={ROUTE_HREF.NOT_FOUND} />;
   }
 
   return <Outlet />;
--- src/components/public-route.tsx
+++ src/components/public-route.tsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from 'react-router';
 
 import { useSession } from '@/components/session-provider';
+import { ROUTE_HREF } from '@/lib/constants';
 
 export function PublicRoute() {
   const session = useSession();
 
   if (session) {
-    return <Navigate to="/albums" replace />;
+    return <Navigate to={ROUTE_HREF.TOP_ALBUMS} replace />;
   }
 
   return <Outlet />;

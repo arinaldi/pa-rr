694529115cc6f836263e30a58a014077650c952c
Tony <arinaldi@vt.edu>
2026-02-21 07:26:39 -0600

    harden loading state for all-time rankings

--- src/routes/albums/all-time-rankings.tsx
+++ src/routes/albums/all-time-rankings.tsx
@@ -22,41 +22,45 @@ export default function AllTimeRankings() {
           <Button>Edit</Button>
         </Link>
       )}
-      <Card>
-        <CardContent>
-          <ol className="ml-4 list-decimal">
-            {favorites.map((f, index) => {
-              const query = encodeURI(`${f.artist} ${f.title}`);
-              const url = `${SPOTIFY_URL}/${query}`;
+      {favorites.length > 0 && (
+        <>
+          <Card>
+            <CardContent>
+              <ol className="ml-4 list-decimal">
+                {favorites.map((f, index) => {
+                  const query = encodeURI(`${f.artist} ${f.title}`);
+                  const url = `${SPOTIFY_URL}/${query}`;
 
-              return (
-                <Fragment key={f.id}>
-                  <li
-                    className={cn(
-                      'text-muted-foreground text-sm',
-                      index > 0 && 'mt-1',
-                    )}
-                  >
-                    <span>{f.artist} &ndash;</span>{' '}
-                    <a
-                      className="text-foreground hover:text-muted-foreground underline underline-offset-4"
-                      href={url}
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      {f.title}
-                    </a>
-                  </li>
-                  {(index + 1) % 10 === 0 && favorites[index + 1] && (
-                    <Separator className="my-4" />
-                  )}
-                </Fragment>
-              );
-            })}
-          </ol>
-        </CardContent>
-      </Card>
-      <TopLink />
+                  return (
+                    <Fragment key={f.id}>
+                      <li
+                        className={cn(
+                          'text-muted-foreground text-sm',
+                          index > 0 && 'mt-1',
+                        )}
+                      >
+                        <span>{f.artist} &ndash;</span>{' '}
+                        <a
+                          className="text-foreground hover:text-muted-foreground underline underline-offset-4"
+                          href={url}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          {f.title}
+                        </a>
+                      </li>
+                      {(index + 1) % 10 === 0 && favorites[index + 1] && (
+                        <Separator className="my-4" />
+                      )}
+                    </Fragment>
+                  );
+                })}
+              </ol>
+            </CardContent>
+          </Card>
+          <TopLink />
+        </>
+      )}
     </div>
   );
 }

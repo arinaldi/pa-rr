52666a36d911c2d742dca6547ab5254ef06b7901
Tony Rinaldi <tony.rinaldi13@gmail.com>
2026-03-17 14:39:29 -0500

    add build options to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,39 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 750,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          minSize: 20_000,
+          groups: [
+            {
+              name: 'react-vendor',
+              test: /node_modules[\\/]react/,
+              priority: 20,
+            },
+            {
+              name: 'ui-vendor',
+              test: /node_modules[\\/]radix/,
+              priority: 15,
+            },
+            {
+              name: 'vendor',
+              test: /node_modules/,
+              priority: 10,
+            },
+            {
+              name: 'common',
+              minShareCount: 2,
+              minSize: 10_000,
+              priority: 5,
+            },
+          ],
+        },
+      },
+    },
+  },
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },

89bbbf169e297c3c5c4c3efc5e5378936a70197f
Tony Rinaldi <tony.rinaldi13@gmail.com>
2026-04-23 10:09:24 -0500

    redirect to /admin on first load

--- src/components/session-provider.tsx
+++ src/components/session-provider.tsx
@@ -5,9 +5,11 @@ import {
   useState,
   type PropsWithChildren,
 } from 'react';
+import { useLocation, useNavigate } from 'react-router';
 import { type Session } from '@supabase/supabase-js';
 
 import { Fallback } from '@/components/fallback';
+import { ROUTES_ADMIN } from '@/lib/constants';
 import { supabase } from '@/supabase/client';
 
 interface Context {
@@ -16,23 +18,20 @@ interface Context {
 
 const SessionContext = createContext<Context>({ session: null });
 
-export function useSession() {
-  const context = use(SessionContext);
-
-  if (!context) {
-    throw new Error('useSession must be used within a SessionProvider');
-  }
-
-  return context.session;
-}
-
 export function SessionProvider({ children }: PropsWithChildren) {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
+    supabase.auth.getSession().then(({ data }) => {
+      setSession(data.session);
+
+      if (data.session && pathname === '/') {
+        navigate(ROUTES_ADMIN.base.href);
+      }
+
       setLoading(false);
     });
 
@@ -45,6 +44,7 @@ export function SessionProvider({ children }: PropsWithChildren) {
     return () => {
       subscription.unsubscribe();
     };
+    // eslint-disable-next-line @eslint-react/exhaustive-deps
   }, []);
 
   return (
@@ -52,6 +52,16 @@ export function SessionProvider({ children }: PropsWithChildren) {
       {loading ? <Fallback /> : children}
     </SessionContext>
   );
+}
+
+export function useSession() {
+  const context = use(SessionContext);
+
+  if (!context) {
+    throw new Error('useSession must be used within a SessionProvider');
+  }
+
+  return context.session;
 }
 
 export type { Session };
